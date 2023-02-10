69017eb8766866b93ebc94aadef725c0da3545b9
sugandh.mishra <sugmishra@microsoft.com>
2023-02-10 09:45:11 +0530

    [ADA-2035] API CI/CD fix

--- core-infrastructure/terraform/modules/containerRegistry/main.tf
+++ core-infrastructure/terraform/modules/containerRegistry/main.tf
@@ -7,9 +7,9 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = var.acr_sku
+  tags                = var.tags
   admin_enabled       = true
 
-  tags = var.tags
   identity {
     type = "SystemAssigned"
   }
